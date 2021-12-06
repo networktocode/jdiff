70c680a4dc24bbea58a3e95786fb1faeed368d8d
Network to Code <federico.olivieri@networktocode.com>
2021-12-06 10:00:23 +0100

    fix dict return in jmespath

--- netcompare/runner.py
+++ netcompare/runner.py
@@ -6,8 +6,6 @@ from .utils.jmspath_parsers import jmspath_value_parser, jmspath_refkey_parser
 from .utils.filter_parsers import exclude_filter
 from .utils.refkey import keys_cleaner, keys_values_zipper, associate_key_of_my_value
 from .utils.flatten import flatten_list
-import pdb
-
 def extract_values_from_output(value: Mapping, path: Mapping, exclude: List) -> Union[Mapping, List, int, str, bool]:
     """Return data from output depending on the check path. See unit text for complete example.
 
@@ -29,38 +27,35 @@ def extract_values_from_output(value: Mapping, path: Mapping, exclude: List) -> 
     """
     # Get the wanted values to be evaluated if jmspath expression is defined, otherwise
     # use the entire output if jmespath is not defined in check. This cover the "raw" diff type.
-    if path:
+    if path and not exclude:
         wanted_value = jmespath.search(jmspath_value_parser(path), value)
-    # RAW diff mode
-    else:
+    
+    elif path and exclude:
+        wanted_value = jmespath.search(jmspath_value_parser(path), value)
+        exclude_filter(wanted_value, exclude)
+    elif not path and exclude:
+        exclude_filter(value, exclude)
         return value
 
-    # Exclude filter implementation.
-    if exclude:
-        exclude_filter(wanted_value, exclude)
-
-    # check if list of lists
-    if not any(isinstance(i, list) for i in wanted_value):
-        raise TypeError(
-            "Catching value must be defined as list in jmespath expression i.e. result[*].state -> result[*].[state]. You have {}'.".format(
-                path
-            )
-        )
-
-    for element in wanted_value:
-        for item in element:
-            if isinstance(item, dict):
-                raise TypeError(
-                    'Must be list of lists i.e. [["Idle", 75759616], ["Idle", 75759620]]. You have {}\'.'.format(
-                        wanted_value
-                    )
-                )
-            elif isinstance(item, list):
-                flatten_list(wanted_value)
-                break
-    
+    # data type check
     if path:
-        paired_key_value = associate_key_of_my_value(jmspath_value_parser(path), wanted_value)
+        if not any(isinstance(i, list) for i in wanted_value):
+            return wanted_value
+
+        for element in wanted_value:
+            for item in element:
+                if isinstance(item, dict):
+                    raise TypeError(
+                        'Must be list of lists i.e. [["Idle", 75759616], ["Idle", 75759620]]. You have {}\'.'.format(
+                            wanted_value
+                        )
+                    )
+                elif isinstance(item, list):
+                    flatten_list(wanted_value)
+                    break
+        
+        
+            paired_key_value = associate_key_of_my_value(jmspath_value_parser(path), wanted_value)
     else:
         paired_key_value = value
 
