eb4ae294cf3357eead3834ef86a36a27ea87e9eb
Patryk Szulczewski <patryk.szulczewski@networktocode.com>
2022-09-22 12:54:52 +0200

    Fix operator checks to follow other check_type logic.

--- tests/test_operators.py
+++ tests/test_operators.py
@@ -39,20 +39,13 @@ operator_all_same = (
                 }
             },
         ],
-        True,
+        False,
     ),
 )
 operator_contains = (
     "pre.json",
     "operator",
     {"params": {"mode": "contains", "operator_data": "EVPN"}},
-    "result[0].vrfs.default.peerList[*].[$peerAddress$,peerGroup]",
-    ([{"7.7.7.7": {"peerGroup": "EVPN-OVERLAY-SPINE"}}], False),
-)
-operator_not_contains = (
-    "pre.json",
-    "operator",
-    {"params": {"mode": "not-contains", "operator_data": "EVPN"}},
     "result[0].vrfs.default.peerList[*].[$peerAddress$,peerGroup]",
     (
         [
@@ -63,55 +56,43 @@ operator_not_contains = (
         False,
     ),
 )
+operator_not_contains = (
+    "pre.json",
+    "operator",
+    {"params": {"mode": "not-contains", "operator_data": "EVPN"}},
+    "result[0].vrfs.default.peerList[*].[$peerAddress$,peerGroup]",
+    (
+        [
+            {"7.7.7.7": {"peerGroup": "EVPN-OVERLAY-SPINE"}},
+        ],
+        False,
+    ),
+)
 operator_is_gt = (
     "pre.json",
     "operator",
     {"params": {"mode": "is-gt", "operator_data": 20}},
     "result[0].vrfs.default.peerList[*].[$peerAddress$,prefixesSent]",
-    (
-        [
-            {"7.7.7.7": {"prefixesSent": 50}},
-            {"10.1.0.0": {"prefixesSent": 50}},
-            {"10.2.0.0": {"prefixesSent": 50}},
-            {"10.64.207.255": {"prefixesSent": 50}},
-        ],
-        False,
-    ),
+    ([], True),
 )
 operator_is_lt = (
     "pre.json",
     "operator",
     {"params": {"mode": "is-lt", "operator_data": 60}},
     "result[0].vrfs.default.peerList[*].[$peerAddress$,prefixesSent]",
-    (
-        [
-            {"7.7.7.7": {"prefixesSent": 50}},
-            {"10.1.0.0": {"prefixesSent": 50}},
-            {"10.2.0.0": {"prefixesSent": 50}},
-            {"10.64.207.255": {"prefixesSent": 50}},
-        ],
-        False,
-    ),
+    ([], True),
 )
 operator_is_in = (
     "pre.json",
     "operator",
     {"params": {"mode": "is-in", "operator_data": [20, 40, 50]}},
     "result[0].vrfs.default.peerList[*].[$peerAddress$,prefixesSent]",
-    (
-        [
-            {"7.7.7.7": {"prefixesSent": 50}},
-            {"10.1.0.0": {"prefixesSent": 50}},
-            {"10.2.0.0": {"prefixesSent": 50}},
-            {"10.64.207.255": {"prefixesSent": 50}},
-        ],
-        False,
-    ),
+    ([], True),
 )
 operator_not_in = (
     "pre.json",
     "operator",
-    {"params": {"mode": "not-in", "operator_data": [20, 40, 60]}},
+    {"params": {"mode": "not-in", "operator_data": [20, 40, 50]}},
     "result[0].vrfs.default.peerList[*].[$peerAddress$,prefixesSent]",
     (
         [
@@ -128,20 +109,12 @@ operator_in_range = (
     "operator",
     {"params": {"mode": "in-range", "operator_data": (20, 60)}},
     "result[0].vrfs.default.peerList[*].[$peerAddress$,prefixesSent]",
-    (
-        [
-            {"7.7.7.7": {"prefixesSent": 50}},
-            {"10.1.0.0": {"prefixesSent": 50}},
-            {"10.2.0.0": {"prefixesSent": 50}},
-            {"10.64.207.255": {"prefixesSent": 50}},
-        ],
-        False,
-    ),
+    ([], True),
 )
 operator_not_in_range = (
     "pre.json",
     "operator",
-    {"params": {"mode": "not-in-range", "operator_data": (20, 40)}},
+    {"params": {"mode": "not-in-range", "operator_data": (20, 60)}},
     "result[0].vrfs.default.peerList[*].[$peerAddress$,prefixesSent]",
     (
         [
@@ -174,7 +147,63 @@ def test_operator(filename, check_type_str, evaluate_args, path, expected_result
     # There is not concept of "pre" and "post" in operator.
     data = load_json_file("api", filename)
     value = extract_data_from_json(data, path)
+    print(evaluate_args, value)
     actual_results = check.evaluate(evaluate_args, value)
     assert actual_results == expected_result, ASSERT_FAIL_MESSAGE.format(
         output=actual_results, expected_output=expected_result
     )
+
+
+@pytest.mark.parametrize(
+    "value, operator_data, expected_result",
+    [
+        (
+            [
+                {"7.7.7.7": {"peerGroup": "EVPN-OVERLAY-SPINE", "vrf": "default", "state": "Connected"}},
+                {"10.1.0.0": {"peerGroup": "EVPN-OVERLAY-SPINE", "vrf": "default", "state": "Connected"}},
+            ],
+            True,
+            ([], True),
+        ),
+        (
+            [
+                {"7.7.7.7": {"peerGroup": "EVPN-OVERLAY-SPINE", "vrf": "default", "state": "Connected"}},
+                {"10.1.0.0": {"peerGroup": "IPv4-UNDERLAY-SPINE", "vrf": "default", "state": "Connected"}},
+            ],
+            True,
+            (
+                [
+                    {"7.7.7.7": {"peerGroup": "EVPN-OVERLAY-SPINE", "vrf": "default", "state": "Connected"}},
+                    {"10.1.0.0": {"peerGroup": "IPv4-UNDERLAY-SPINE", "vrf": "default", "state": "Connected"}},
+                ],
+                False,
+            ),
+        ),
+        (
+            [
+                {"7.7.7.7": {"peerGroup": "EVPN-OVERLAY-SPINE", "vrf": "default", "state": "Connected"}},
+                {"10.1.0.0": {"peerGroup": "EVPN-OVERLAY-SPINE", "vrf": "default", "state": "Connected"}},
+            ],
+            False,
+            (
+                [
+                    {"7.7.7.7": {"peerGroup": "EVPN-OVERLAY-SPINE", "vrf": "default", "state": "Connected"}},
+                    {"10.1.0.0": {"peerGroup": "EVPN-OVERLAY-SPINE", "vrf": "default", "state": "Connected"}},
+                ],
+                False,
+            ),
+        ),
+        (
+            [
+                {"7.7.7.7": {"peerGroup": "EVPN-OVERLAY-SPINE", "vrf": "default", "state": "Connected"}},
+                {"10.1.0.0": {"peerGroup": "IPv4-UNDERLAY-SPINE", "vrf": "default", "state": "Connected"}},
+            ],
+            False,
+            ([], True),
+        ),
+    ],
+)
+def test_all_same(value, operator_data, expected_result):
+    check_args = {"params": {"mode": "all-same", "operator_data": operator_data}}
+    check = CheckType.create("operator")
+    assert check.evaluate(check_args, value) == expected_result
