bc3504f67b0e901dd0c415e0b4396c2fb44c5d6e
Network to Code <federico.olivieri@networktocode.com>
2023-03-22 10:31:35 +0100

    update certificate model

--- lb_models/choices.py
+++ lb_models/choices.py
@@ -1,29 +1,6 @@
 """Choices for LB models."""
 from nautobot.utilities.choices import ChoiceSet
 
-
-class CertAlgorithmChoices(ChoiceSet):
-    """Valid choices of certificate signing algorithm."""
-
-    SHA1 = "sha1"
-    SHA224 = "sha224"
-    SHA256 = "sha256"
-    SHA384 = "sha384"
-    SHA512 = "sha512"
-    MD2 = "md2"
-    MD5 = "md5"
-
-    CHOICES = (
-        (SHA1, "sha1"),
-        (SHA224, "sha224"),
-        (SHA256, "sha256"),
-        (SHA384, "sha384"),
-        (SHA512, "sha512"),
-        (MD2, "md2"),
-        (MD5, "md5"),
-    )
-
-
 class Protocols(ChoiceSet):
     """Valid choices for protocols."""
 
